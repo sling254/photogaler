ecece2c4a326f7c0743c8e2ff4d90094491b667a
sling254 <mwanikidenis7@gmail.com>
2021-11-29 01:17:00 +0300

    Refactor index.html

--- photogaler/static/js/index.js
+++ photogaler/static/js/index.js
@@ -1,13 +1,10 @@
-function imgWindow() {
-    window.open("image")
-  }
   $(document).ready(function () {
     $(function () {
       $('.photoUrl').click(function () {
         $(this).focus();
         $(this).select();
         document.execCommand('copy');
-        alert("Copied to clipboard");
+        alert("Link Copied to clipboard");
       });
     });
   });
