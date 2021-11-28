2d58bece1b5782c62a1cc6db60685d8d8d5e6160
sling254 <mwanikidenis7@gmail.com>
2021-11-28 16:18:50 +0300

    add a search view add a post method and action to the form

--- photogaler/gallery/urls.py
+++ photogaler/gallery/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView,searched_images
 
 urlpatterns = [
     path('',IndexView,name="index"),
+    path('',searched_images,name="searched_images"),
 ]
