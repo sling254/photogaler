46d57c28f8ac5a90b89b65fe7501b058ebe64257
sling254 <mwanikidenis7@gmail.com>
2021-11-28 16:12:51 +0300

    register the models to admin.py and add secondary class to the submit btn

--- photogaler/gallery/admin.py
+++ photogaler/gallery/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category, Image, Location 
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Image)
+admin.site.register(Location)
